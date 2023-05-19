37163a90aa7f8403a3363ee7c3590d052918e06b
asabale221 <119834965+asabale221@users.noreply.github.com>
2023-05-19 18:21:30 +0600

    users create urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,5 @@
 """
 URL configuration for core project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
@@ -15,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+api_urlpatterns = [
+    path('users/', include('apps.users.urls')),
+    path('todo/', include('apps.todo.urls')),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(api_urlpatterns))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
